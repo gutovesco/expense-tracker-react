02fd73b8c167f699a46f45629b1c65bca07535f4
Augusto Raveli <guto.vesco@hotmail.com>
2020-03-05 14:30:27 -0300

    added context

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import {IncomeExpenses} from './components/IncomeExpenses';
 import {TransactionList} from './components/transactionList';
 import {AddTransaction} from './components/addTransaction';
 
+let Context = React.createContext();
+
 function App() {
+  let [currentUser, setCurrentUser] = React.useState('');
   return (
+  <Context.Provider value={{ currentUser }}>
     <div>
       <Header/>
       <div className="container">
@@ -15,9 +19,26 @@ function App() {
         <IncomeExpenses/>
         <TransactionList/>
         <AddTransaction/>
+        <LoginScreen onLogin={() => setCurrentUser({ name: "Juquinha"})}/>
+        <ShowMessage/>
       </div>
     </div>
+  </Context.Provider>
   );
 }
 
+function LoginScreen({onLogin}){
+  return(
+    <button onClick={ onLogin }>Login</button>
+  )
+}
+
+function ShowMessage(){
+  let { currentUser } = React.useContext(Context);
+  return(
+    <div>
+      <p> Welcome { currentUser.name } </p>
+    </div>
+  )
+}
 export default App;
