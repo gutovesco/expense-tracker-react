3a4c6e8acef2636a01fda47c3320c5c5ab117855
Augusto Raveli <guto.vesco@hotmail.com>
2020-03-05 10:55:46 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/header';
+import {Balance} from './components/balance';
+import {IncomeExpenses} from './components/IncomeExpenses';
+import {TransactionList} from './components/transactionList';
+import {AddTransaction} from './components/addTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
